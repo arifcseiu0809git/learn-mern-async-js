48d4ddf24cf10e0e0fa9c10a5ae3f16d24c602d8
arifcseiu0809git <114827681+arifcseiu0809git@users.noreply.github.com>
2023-07-29 21:43:50 +0600

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 import './style.css';
 
 // Write Javascript code!
+console.log('Hello world');
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Lern mern like a pro</h1>`;
